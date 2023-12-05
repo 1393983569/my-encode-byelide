3bc1f2faad780067fb67658439821ac2025041cc
v_liukaijie <v_liukaijie@digitalgd.com.cn>
2023-12-05 11:51:28 +0800

    feat: init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+const aa = 4
+const bb = 4
+aa == bb
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
